7a41e413a1305617794d160d30a7c994d1dff087
Emmanuel <rodrigofemi@gmail.com>
2024-11-14 11:52:33 +0100

    Base Apparel

--- src/Components/Apparel.jsx
+++ src/Components/Apparel.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import logo from "../assets/images/logo.svg"//gradient-to-r import bg from "../assets/images/bg-pattern-desktop.svg";
 import err from "../assets/images/icon-error.svg";
 import arrow from "../assets/images/icon-arrow.svg";
-import img from "../assets/images/hero-desktop.jpg";
+import desktop from "../assets/images/hero-desktop.jpg";
+import mobile from "../assets/images/hero-mobile.jpg";
 import PropTypes from "prop-types";
 
 export default function Apparel({ email, onChangeEmail, onSubmitEmail }) {
@@ -26,12 +27,12 @@ export default function Apparel({ email, onChangeEmail, onSubmitEmail }) {
     }
   
   return (
-    <div className=" h-screen w-full font-body">
-      <div className="flex flex-row rounded-sm  h-full">
+    <div className=" md:h-screen sm:h-full w-full font-body">
+      <div className="flex md:flex-row sm:flex-col-reverse rounded-sm  h-full">
         
-        <div className="py-16 px-24 w-1/2 bg-gradient-to-r from-left-image to-left-image bg-cover bg-right">
-          <div className="mb-9">
-            <img className="w-24" src={logo} alt="" />
+        <div className="md:py-16 sm:py-20 px-24 md:w-1/2 bg-gradient-to-r sm:text-center md:text-start from-left-image to-left-image bg-cover bg-right">
+          <div className="sm:hidden md:block mb-9">
+            <img className="w-24 " src={logo} alt="" />
           </div>
 
           <div>
@@ -63,15 +64,21 @@ export default function Apparel({ email, onChangeEmail, onSubmitEmail }) {
                   <img className="w-4" src={arrow} alt="" />
                 </button>
               </div>
-                  {error && (<p className="text-primary-softred text-xs font-light -mt-8 ml-4">{error}</p>)}
+                  {error && (<p className="text-primary-softred text-xs font-light -mt-8 lml-4 flex justify-start">{error}</p>)}
             </form>
           </div>
           
         </div>
 
-        <div className="w-1/2">
-          <div className="h-full">
-            <img className="w-full h-full" src={img} alt="" />
+        <div className="md:w-1/2">
+          <div className="sm:block md:hidden p-10">
+            <img className="w-24 " src={logo} alt="" />
+          </div>
+          <div className="h-full sm:hidden md:block">
+            <img className="w-full h-full" src={desktop} alt="" />
+          </div>
+          <div className="h-full sm:block md:hidden">
+            <img className="w-full h-full" src={mobile} alt="" />
           </div>
         </div>
 
